10a6d0b96c2168d3aea626db909984586f2f8673
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-08-11 11:03:10 +0200

    test update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
+import { AppController } from '../src/app.controller';
+import { MonitorService } from './monitor.service';
+import { HttpModule } from '@nestjs/common';
+import { LoggingModule } from 'logging-module';
+import { CpuEventsGateway } from './cpu-events.gateway';
 
-describe('App Controller', () => {
-  let controller: AppController;
+describe('AppController', () => {
+  let appController: AppController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule,
+        LoggingModule
+      ],
       controllers: [AppController],
+      providers: [
+        MonitorService,
+        CpuEventsGateway,
+      ],
     }).compile();
 
-    controller = module.get<AppController>(AppController);
+    appController = app.get<AppController>(AppController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(appController).toBeDefined();
   });
+
 });
+
+
+// describe('App Controller', () => {
+//   let controller: AppController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [AppController],
+//       providers: [
+//         MonitorService,
+//         CpuEventsGateway,
+//       ],
+//     }).compile();
+
+//     console.log("?????????")
+//     controller = module.get<AppController>(AppController);
+//   });
+
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
