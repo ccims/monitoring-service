0d281d8cd6a223b516165d3b30e4bc02de0ca052
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2020-08-13 13:33:02 +0200

    added 'todo' to e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,14 +4,5 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  test.todo("todo");
 });
