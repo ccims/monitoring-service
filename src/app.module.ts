821230ec43c8443f4b6497789216ac5f21472eb2
Hungergefuel <45015016+Hungergefuel@users.noreply.github.com>
2020-08-16 19:42:44 +0200

    Remove initial endpoint
    Tried moving initial endpoint to config but didnt work

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MonitorService } from './monitor.service';
 import { CpuEventsGateway } from './cpu-events.gateway';
-import { LoggingModule } from 'logging-module'
+import { LoggingModule } from 'logging-module';
+import { ConfigModule } from '@nestjs/config';
+ 
 
 @Module({
   imports: [
     HttpModule,
-    LoggingModule
+    LoggingModule,
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [
